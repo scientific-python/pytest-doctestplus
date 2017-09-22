78e0e8aef8ce0fbdb3e7656c9f4d443a9fdca574
Dan D'Avella <drdavella@gmail.com>
2017-09-22 13:24:05 -0400

    Add missing setup.py, do not use 'src' directory

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- encoding: utf-8 -*-
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pytest-doctestplus',
+    version='0.1.0',
+    license='BSD',
+    description='Pytest plugin with advanced doctest features.',
+    author='The Astropy Developers',
+    author_email='astropy.team@gmail.com',
+    url='https://astropy.org',
+    packages=find_packages(),
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    keywords=[ 'doctest', 'rst', 'pytest', 'py.test' ],
+    install_requires=[ 'pytest>=2.8.0' ],
+    python_requires='>=2.7',
+    entry_points={
+        'pytest11': [
+            'pytest_doctestplus = pytest_doctestplus.plugin',
+        ],
+    },
+)
