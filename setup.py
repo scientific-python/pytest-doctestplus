2ac04330deb60e85c5a7ee8ddd1539011c4dc91a
Dan D'Avella <drdavella@gmail.com>
2017-09-25 15:43:23 -0400

    Add dependency on six to install_requires

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Topic :: Utilities',
     ],
     keywords=[ 'doctest', 'rst', 'pytest', 'py.test' ],
-    install_requires=[ 'pytest>=2.8.0' ],
+    install_requires=[ 'six', 'pytest>=2.8.0' ],
     python_requires='>=2.7',
     entry_points={
         'pytest11': [
