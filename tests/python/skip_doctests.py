484dedfe86dd272294e5e0129fdc965b4f017770
Dan D'Avella <ddavella@stsci.edu>
2017-10-04 15:48:02 -0400

    Add test cases to exercise doctestplus test discovery

---
+++ tests/python/skip_doctests.py
@@ -0,0 +1,31 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Skip all tests in this file since they'll all fail
+__doctest_skip__ = ['*']
+
+
+def bad_doctest():
+    """
+    This test will fail if __doctest_skip__ is not working properly.
+
+    >>> x + y
+    5
+    """
+
+def another_bad_doctest():
+    """
+    This test will fail if __doctest_skip__ is not working properly.
+
+    >>> import foobar
+    >>> foobar.baz()
+    5
+    """
+
+def yet_another_bad_doctest():
+    """
+    This test will fail if __doctest_skip__ is not working properly.
+    
+    >>> NOT VALID PYTHON, RIGHT
+    >>> + 7
+    42
+    """
