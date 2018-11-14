49044b09bcaeff8206facbfd6737162a4e3165b6
Daniel D'Avella <drdavella@gmail.com>
2018-11-14 14:41:14 -0500

    Add new section to change log and bump version in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def readme():
 
 setup(
     name='pytest-doctestplus',
-    version='0.1.4.dev',
+    version='0.3.0.dev',
     license='BSD',
     description='Pytest plugin with advanced doctest features.',
     long_description=readme(),
