83d93a7042725a3bfba54af012e69e04dd806a72
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2019-01-04 17:49:44 +0000

    Add tests for FLOAT_CMP

--- tests/test_doctestplus.py
+++ tests/test_doctestplus.py
@@ -6,6 +6,7 @@ def test_ignored_whitespace(testdir):
         """
         [pytest]
         doctest_optionflags = ELLIPSIS NORMALIZE_WHITESPACE
+        doctestplus = enabled
     """
     )
     p = testdir.makepyfile(
@@ -28,6 +29,7 @@ def test_non_ignored_whitespace(testdir):
         """
         [pytest]
         doctest_optionflags = ELLIPSIS
+        doctestplus = enabled
     """
     )
     p = testdir.makepyfile(
@@ -43,3 +45,57 @@ def test_non_ignored_whitespace(testdir):
     )
     reprec = testdir.inline_run(p, "--doctest-plus")
     reprec.assertoutcome(failed=1, passed=0)
+
+
+def test_float_cmp(testdir):
+    testdir.makeini(
+        """
+        [pytest]
+        doctest_optionflags = ELLIPSIS
+        doctestplus = enabled
+    """
+    )
+    p = testdir.makepyfile(
+        """
+        def f():
+            '''
+            >>> x = 1/3.
+            >>> x
+            0.333333
+            '''
+            pass
+
+        def g():
+            '''
+            >>> x = 1/3.
+            >>> x    # doctest: +FLOAT_CMP
+            0.333333
+            '''
+            pass
+    """
+    )
+    reprec = testdir.inline_run(p, "--doctest-plus")
+    reprec.assertoutcome(failed=1, passed=1)
+
+
+def test_float_cmp_global(testdir):
+    testdir.makeini(
+        """
+        [pytest]
+        doctest_optionflags = FLOAT_CMP
+        doctestplus = enabled
+    """
+    )
+    p = testdir.makepyfile(
+        """
+        def f():
+            '''
+            >>> x = 1/3.
+            >>> x
+            0.333333
+            '''
+            pass
+    """
+    )
+    reprec = testdir.inline_run(p, "--doctest-plus")
+    reprec.assertoutcome(passed=1)
