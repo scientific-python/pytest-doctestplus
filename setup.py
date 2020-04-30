1486107391f3118b2353e877cc17cc9f43c4af6f
Brigitta Sipocz <bsipocz@gmail.com>
2020-04-30 00:00:13 -0700

    Back to development: v0.6.1.dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
     name='pytest-doctestplus',
-    version='0.6.0',
+    version='0.6.1.dev0',
     license='BSD',
     description='Pytest plugin with advanced doctest features.',
     long_description=readme(),
